a97a899197b839beebd1b049e97eb14542ca9d01
Alok Chandra <alokchandra2611@gmail.com>
2026-09-03 08:40:18 +0530

    Remove unused Services section and styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Hero from '../components/sections/Hero.jsx';
-import Services from '../components/sections/Services.jsx';
 import Work from '../components/sections/Work.jsx';
 import About from '../components/sections/About.jsx';
 import Education from '../components/sections/Education.jsx';
@@ -12,7 +11,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Services />
       <Work />
       <About />
       <Education />
